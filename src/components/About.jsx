5f0591baa839b4eca3684fae48419ffd1813f6e9
Nacho Hotz <juanhotz611@gmail.com>
2021-07-29 17:30:54 -0300

    Update About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,7 @@ import "./styles/About.css";
 
 export default function About() {
   return <div id="container">
-        <h5>App made by Juan Ignacio Hotz for the Henry Bootcamp.</h5>
-        <h6>Tecnologies used:</h6>
+        <h5>App made the following technologies.</h5>
         <ul>
             <li>HTML</li>
             <li>CSS</li>
